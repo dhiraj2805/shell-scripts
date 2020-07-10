39a7ad10e8743678b54015bc80287eef5522e8b8
dhiraj <dhielec1999@gmail.com>
2020-07-09 19:09:03 -0600

    thank you

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 echo hello world2
 echo -e "hello, \nwellcome to canada, \nhow are ya"
+echo -e "\e[32mHello\e[0m"
+echo -e "\e[34mGood to see you\e[0m"
+echo -e "\e[36mCool mach\e[0m"
