cf9f5b06863629f72e087a3c198fe2a5fb117835
dhiraj <dhielec1999@gmail.com>
2020-07-22 20:36:06 -0600

    Seed Job

--- newjob.groovy
+++ newjob.groovy
@@ -1,4 +1,14 @@
-pipelineJob("newjob") {
+folder('Roboshop') {
+	displayname('Roboshop')
+	description('Roboshop Project Folder')
+}
+folder('Roboshop/CIJOBS') {
+	displayname('Roboshop/CIJOBS')
+	description('Roboshop Project CI JOBS Folder')
+}
+
+
+pipelineJob("Roboshop/CIJOBS/newjob") {
 	description('newjob')
 	definition {
 		cpsScm {
